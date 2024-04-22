4f4f6ab7fb780d7dd5de39f3bf3dc29c5fe47f95
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-22 14:43:54 -0300

    Add project files.

---
+++ VetoresParesImpares/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] numeros = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Digite o {i}° caracter: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+
+}
+Console.WriteLine();
+Console.Write("Pares: ");
+
+for (int i = 0; i < 10; i++)
+{
+    if (numeros[i] % 2 == 0)
+    {
+        Console.Write(numeros[i] + " ");
+    }
+}
+Console.WriteLine();
+Console.Write("Impares: ");
+for (int i = 0; i < 10; i++)
+{
+    if (numeros[i] % 2 != 0)
+    {
+        Console.Write(numeros[i] + " ");
+    }
+}
+Console.WriteLine();
+for (int i = 0;i <10; i++)
+{
+    if (i == 0 || i == 9)
+    {
+        Console.Write(numeros[i] + " ");
+    }
+}
